29d628ab2b3a9c6ed56eb990e85b575b7d11c69a
Jaakko-Heikki Heusala <jheusala@iki.fi>
2013-05-17 17:27:59 +0300

    Updated

---
+++ src/check.js
@@ -0,0 +1,22 @@
+
+/* Checks for types */
+
+var is = module.exports = {};
+
+/** Returns true if object is an object */
+is.obj = function is_obj(o) {
+	return (o && (typeof o === 'object')) ? true : false;
+};
+
+/** Returns true if object is an array */
+is.array = function is_array(o) {
+	return (is_obj(o) && (o instanceof Array)) ? true : false;
+};
+
+/** Returns true if object is a function */
+is.fun = function is_fun(o) {
+	if(is_obj(o) && (o instanceof Function) ) return true;
+	return (o && (typeof o === 'function')) ? true : false;
+};
+
+/* EOF */
